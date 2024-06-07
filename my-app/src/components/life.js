493d0e300aba93af8f182d5c3ca21e08282feb60
Ahmad Malkawi <malkawi_ahmad@hotmail.com>
2024-06-07 08:24:34 +0300

    Update life.js

--- my-app/src/components/life.js
+++ my-app/src/components/life.js
@@ -1,8 +1,79 @@
-import React,{Component} from "react";
-import '../Css/life.css';
+// import React,{Component} from "react";
+// import '../Css/life.css';
 
 
-class LifecycleDemo extends Component{
+// class LifecycleDemo extends Component{
+//     constructor(probs){
+//         super(probs)
+//         this.state = {
+//             data:null,
+//             loading:true,
+//         }
+//         console.log('constructor')
+//     }
+
+//     componentWillMount(){
+//         console.log('componentWillMount')
+//     }
+
+//     componentDidMount(){
+//         console.log('componentDidMount')
+//         this.fetchData()
+//     }
+
+//     fetchData(){
+//         setTimeout(()=>{
+//             this.setState({
+//                 data:[{id:1,name:'AHMAD'},{id:2,name:'MOHAMMAD'}],
+//                 loading:false,
+//             })
+//             console.log('it fetch the data')
+//         },2000)
+       
+
+//     }
+
+//     render(){
+//         console.log('render')
+//         const {data,loading} = this.state;
+//         return(
+//             <>
+//                 <h1>life cycle demo</h1>
+//                 {loading?<h1>loading....</h1>:<h1>{this.renderTable(data)}</h1>}
+//             </>
+//         )
+
+//     }
+
+//     renderTable(tableData){
+//         return(
+//             <table>
+//                 <thead>
+//                     <tr>
+//                         <th>id</th>
+//                         <th>name</th>
+//                     </tr>
+//                 </thead>
+
+//                 <tbody>
+//                     {tableData.map(item=>(
+//                         <tr>
+//                             <td>{item.id}</td>
+//                             <td>{item.name}</td>
+//                         </tr>
+//                     ))}
+//                 </tbody>
+//             </table>
+//         )
+//     }
+// }
+
+// export default LifecycleDemo;
+
+
+import React,{Component} from "react"
+
+class A extends Component{
     constructor(probs){
         super(probs)
         this.state = {
@@ -12,60 +83,72 @@ class LifecycleDemo extends Component{
         console.log('constructor')
     }
 
+
     componentWillMount(){
-        console.log('componentWillMount')
+        console.log('component Will Mount')
     }
 
     componentDidMount(){
-        console.log('componentDidMount')
-        this.fetchData()
+        console.log('component Did Mount')
+        this.fetchData();
     }
 
     fetchData(){
         setTimeout(()=>{
             this.setState({
-                data:[{id:1,name:'AHMAD'},{id:2,name:'MOHAMMAD'}],
-                loading:false,
-            })
-            console.log('it fetch the data')
+            data:[{Company:'Alfreds Futterkiste',Contact:'Maria Anders',Country:'Germany'},{Company:'Centro comercial Moctezuma',Contact:'Francisco Chang',Country:'Mexico'}],
+            loading:false,
+        
+             })  
         },2000)
-       
-
+     
     }
 
     render(){
-        console.log('render')
-        const {data,loading} = this.state;
+        const{data,loading}=this.state
         return(
-            <>
-                <h1>life cycle demo</h1>
-                {loading?<h1>loading....</h1>:<h1>{this.renderTable(data)}</h1>}
-            </>
+        <>
+            {loading?<h1>loading....</h1>:<h1>{this.renderTable(data)}</h1>}
+        </>
         )
-
     }
+
 
     renderTable(tableData){
         return(
-            <table>
-                <thead>
-                    <tr>
-                        <th>id</th>
-                        <th>name</th>
-                    </tr>
-                </thead>
+            <>
+                <table>
+                    <thead>
+                       <tr>
+                            <th>Company</th>
+                            <th>Contact</th>
+                            <th>Country</th>
+                       </tr> 
+                    </thead>
+                    <tbody>
+                        {tableData.map(item=>(
+                            <tr>
+                                <td>{item.Company}</td>
+                                <td>{item.Contact}</td>
+                                <td>{item.Country}</td>
 
-                <tbody>
-                    {tableData.map(item=>(
-                        <tr>
-                            <td>{item.id}</td>
-                            <td>{item.name}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+                            </tr>
+                        ))}
+                    </tbody>
+                  
+                </table>
+            </>
         )
     }
+
+
+
+
+
+
+
+
+
 }
 
-export default LifecycleDemo;
+export default A;
